ea7e363d816d3ab00fc32a7f88ac39d41701fb22
Nguyen Ba Hung <nguyen.ba.hung@framgia.com>
2019-02-14 13:49:12 +0700

    update navigator

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,10 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('List of English words')
+        title: new Text('List of English words'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushToSaveWordsScreen)
+        ],
       ),
       body: new ListView.builder(itemBuilder: (context, index) {
         if (index >= _words.length) {
@@ -27,6 +30,25 @@ class RandomEnglishWordsState extends State<RandomEnglishWords> {
         return _buildRow(_words[index], index);
       })
     );
+  }
+
+  _pushToSaveWordsScreen() {
+    final pageRoute = new MaterialPageRoute(builder: (context) {
+      final listTiles = _checkedWords.map((wordPair) {
+        return new ListTile(
+          title: new Text(wordPair.asUpperCase, style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))
+        );
+      });
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Checked Words")
+        ),
+        body: new ListView(
+          children: listTiles.toList(),
+        )
+      );
+    });
+    Navigator.of(context).push(pageRoute);
   }
 
   Widget _buildRow(WordPair wordPair, int index) {
