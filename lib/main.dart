5ee94ba4a5efae9044dcbfa670be36c809509142
Hung Nguyen Ba <nbhung1809@gmail.com>
2019-02-20 16:28:30 +0700

    finished layout for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-
 
 void main() => runApp(MyApp());
-
-class RandomEnglishWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new RandomEnglishWordsState();
-  }
-}
-
-class RandomEnglishWordsState extends State<RandomEnglishWords> {
-  final _words = <WordPair>[];
-  final _checkedWords = new Set<WordPair>();
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('List of English words'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushToSaveWordsScreen)
-        ],
-      ),
-      body: new ListView.builder(itemBuilder: (context, index) {
-        if (index >= _words.length) {
-          _words.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_words[index], index);
-      })
-    );
-  }
-
-  _pushToSaveWordsScreen() {
-    final pageRoute = new MaterialPageRoute(builder: (context) {
-      final listTiles = _checkedWords.map((wordPair) {
-        return new ListTile(
-          title: new Text(wordPair.asUpperCase, style: new TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))
-        );
-      });
-      return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Checked Words")
-        ),
-        body: new ListView(
-          children: listTiles.toList(),
-        )
-      );
-    });
-    Navigator.of(context).push(pageRoute);
-  }
-
-  Widget _buildRow(WordPair wordPair, int index) {
-    final color = index % 2 == 0 ? Colors.red : Colors.blue;
-    final isChecked = _checkedWords.contains(wordPair);
-    return new ListTile(
-      leading: new Icon(isChecked ? Icons.check_box : Icons.check_box_outline_blank, color: color),
-      title: new Text(
-        wordPair.asUpperCase,
-        style: new TextStyle(fontSize: 18.0, color: color)
-      ),
-      onTap: () {
-        setState(() {
-          if (isChecked) {
-            _checkedWords.remove(wordPair);
-          } else {
-            _checkedWords.add(wordPair);
-          }
-        });
-      },
-    );
-  }
-}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: new Text('Programming tutorials channel', style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 13.0)),
+              ),
+              new Container(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: new Text('This channel contains tutorial video in Flutter, React Native, React.js, Vue.js and so on', style: new TextStyle(fontSize: 11.0))
+              ),
+            ],
+          )),
+          new Container(
+            padding:EdgeInsets.only(right: 2.0),
+            child: new Icon(Icons.favorite, color: Colors.red, size: 19.0),
+          ),
+          new Text('100', style: new TextStyle(fontSize: 12.0))
+        ],
+      ),
+    );
+
+
+    Widget buildButton(IconData buttonIcon, String buttonText) {
+      final Color mainIconColor = Colors.blue;
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              padding: EdgeInsets.only(bottom: 8.0),
+              child: new Icon(buttonIcon, color: mainIconColor)
+            ),
+            new Text(buttonText, style: new TextStyle(color: mainIconColor, fontWeight: FontWeight.w600)),
+          ],
+        ),
+      );
+    }
+
+    Widget fourButtonSection = new Container(
+      padding: EdgeInsets.all(10.0),
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          buildButton(Icons.home, 'Home'),
+          buildButton(Icons.arrow_back, 'Back'),
+          buildButton(Icons.arrow_forward, 'Next'),
+          buildButton(Icons.share, 'Share'),
+        ],
+      ),
+    );
+
+    Widget theFuckingLongText = new Container(
+      padding:EdgeInsets.all(10.0),
+      child: new Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae varius mauris. Nunc dapibus tristique velit ac cursus. In tristique tellus eros, eget congue lacus porttitor sed. Pellentesque erat magna, semper ut finibus in, rutrum nec lacus. Sed gravida vehicula blandit. Nam efficitur felis et metus eleifend, ac feugiat sem accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper purus dolor, at pulvinar magna ullamcorper eu. Ut tristique accumsan risus at porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae varius mauris. Nunc dapibus tristique velit ac cursus. In tristique tellus eros, eget congue lacus porttitor sed. Pellentesque erat magna, semper ut finibus in, rutrum nec lacus. Sed gravida vehicula blandit. Nam efficitur felis et metus eleifend, ac feugiat sem accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper purus dolor, at pulvinar magna ullamcorper eu. Ut tristique accumsan risus at porttitor.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae varius mauris. Nunc dapibus tristique velit ac cursus. In tristique tellus eros, eget congue lacus porttitor sed. Pellentesque erat magna, semper ut finibus in, rutrum nec lacus. Sed gravida vehicula blandit. Nam efficitur felis et metus eleifend, ac feugiat sem accumsan. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper purus dolor, at pulvinar magna ullamcorper eu. Ut tristique accumsan risus at porttitor.')
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
-      home: new RandomEnglishWords()
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter App')
+        ),
+        body: new ListView(
+          children: <Widget>[
+            new Image.asset(
+              'images/image.png',
+              fit: BoxFit.cover
+            ),
+            titleSection,
+            fourButtonSection,
+            theFuckingLongText
+          ],
+        )
+      )
     );
   }
 }
