34180fe9516479a2d2c5e095a0c90264e237a135
Hung Nguyen Ba <nbhung1809@gmail.com>
2019-02-21 22:43:38 +0700

    test git add .

--- lib/group.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Group extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Center( child: new Text('This is Group tab'));
-  }
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,13 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
-import 'chat.dart';
-import 'group.dart';
-import 'cloud.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final tabsController = new DefaultTabController(
-      length: 4,
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('These are tabs'),
-          bottom: new TabBar(
-            tabs: <Widget>[
-              new Tab(icon: new Icon(Icons.home), text: 'Home' ),
-              new Tab(icon: new Icon(Icons.group), text: 'Group' ),
-              new Tab(icon: new Icon(Icons.chat_bubble), text: 'Message' ),
-              new Tab(icon: new Icon(Icons.cloud), text: 'Cloud' )
-            ],
-          ),
-        ),
-        body: new TabBarView(
-          children: <Widget>[
-            new Home(),
-            new Group(),
-            new Chat(),
-            new Cloud()
-          ],
-        ),
-      ),
-    );
     return MaterialApp(
       title: 'Flutter Demo',
-      home: tabsController
+      home: new Text('fuck')
     );
   }
 }
